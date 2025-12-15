530d6501337ff5849fa98c7d7f7cb7b5b1076c39
danteclear <jamesmidzi@gmail.com>
2025-12-15 20:52:44 +0200

    feat: initial layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,27 @@
-<script setup></script>
+<script setup>
+const items = [
+  { label: "Home", to: "/" },
+  { label: "About", to: "/about" },
+];
+</script>
 
 <template>
-	<div>
-		<UHeader title="Crypto Forge" />
-		<UMain>
-			<slot />
-		</UMain>
-	</div>
+  <div>
+    <UHeader title="Crypto Forge">
+      <template #right>
+        <UColorModeButton />
+        <div class="hidden lg:block">
+          <UNavigationMenu :items="items" />
+        </div>
+      </template>
+      <template #body>
+        <UNavigationMenu :items="items" orientation="vertical" />
+      </template>
+    </UHeader>
+    <UMain>
+      <UContainer class="py-12">
+        <slot />
+      </UContainer>
+    </UMain>
+  </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,35 @@
-<script setup></script>
+<script setup>
+const items = ref([5, 10, 20, 30]);
+const value = ref(10);
+
+const sort = ref(["Market Cap (High to Low)", "Market Cap (Low to High)"]);
+const sortValue = ref("Market Cap (High to Low)");
+</script>
 
 <template>
-	<div>index</div>
+  <div class="flex items-center gap-4">
+    <UInput size="xl" class="flex-1" />
+    <div>
+      <Ulabel> Show </Ulabel>
+      <USelectMenu v-model="value" :items="items" size="xl" />
+    </div>
+    <div>
+      <Ulabel> Sort </Ulabel>
+      <USelectMenu v-model="sortValue" :items="sort" size="xl" />
+    </div>
+  </div>
+
+  <div class="grid sm:grid-cols-2 mt-4 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <UCard>
+      <template #header>
+        <h3>Bitcoin</h3>
+        BTC
+      </template>
+      <p>Price: $85,868 -3.43 %</p>
+      <p>Market Cap: 1,710,776,038,829</p>
+    </UCard>
+    <UCard>index</UCard>
+    <UCard>index</UCard>
+    <UCard>index</UCard>
+  </div>
 </template>
